14f7228616b4721e2eb3690eeb16b3121a9c96b7
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 13:34:57 -0300

    Adicionar repositório da categoria

---
+++ src/main/java/br/com/zup/zupcontatos/repositories/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.zupcontatos.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository <CategoriaRepository, Integer> {
+}
