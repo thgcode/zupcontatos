47c12cbbe8d40e4167e9af7706886e80e4222fc2
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 16:57:17 -0300

    Adicionar a primeira versão para atualizar a categoria do produto

--- src/main/java/br/com/zup/zupcontatos/services/ProdutoService.java
+++ src/main/java/br/com/zup/zupcontatos/services/ProdutoService.java
@@ -19,6 +19,12 @@ public class ProdutoService {
     }
 
     public ProdutoModel cadastrarProduto(ProdutoModel produto) {
+        Optional <ProdutoModel> optionalProduto = produtoRepository.findById(produto.getId());
+
+        if (optionalProduto.isPresent()) {
+            return atualizarProdutoComNovaCategoria(optionalProduto.get(), produto);
+        }
+
         CategoriaModel categoria = categoriaService.pesquisarCategoriaPeloId(produto.getListaDeCategorias().get(0).getId());
 
         produto.getListaDeCategorias().set(0, categoria);
@@ -26,6 +32,28 @@ public class ProdutoService {
         return produtoRepository.save(produto);
     }
 
+    private boolean produtoJaTemACategoria(ProdutoModel produto, CategoriaModel categoria) {
+        for (CategoriaModel categoriaPesquisar: produto.getListaDeCategorias()) {
+            if (categoriaPesquisar.getId() == categoria.getId()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private ProdutoModel atualizarProdutoComNovaCategoria(ProdutoModel existente, ProdutoModel novaCategoria) {
+        CategoriaModel categoria = categoriaService.pesquisarCategoriaPeloId(novaCategoria.getListaDeCategorias().get(0).getId());
+
+        if (!produtoJaTemACategoria(existente, categoria)) {
+            existente.getListaDeCategorias().add(categoria);
+
+            return produtoRepository.save(existente);
+        }
+
+        return existente;
+    }
+    
     public Iterable <ProdutoModel> listarProdutos() {
         return produtoRepository.findAll();
     }
