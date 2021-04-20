e25cf881bee0a0cc7b3ec6d1b354363f313748a1
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 13:33:26 -0300

    Adicionado model da categoria

---
+++ src/main/java/br/com/zup/zupcontatos/models/CategoriaModel.java
@@ -0,0 +1,33 @@
+package br.com.zup.zupcontatos.models;
+
+import javax.persistence.*;
+
+@Entity
+public class CategoriaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    public CategoriaModel() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
