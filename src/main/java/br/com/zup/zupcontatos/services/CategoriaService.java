a8aa2c996d4c1555782f248b366be68b0f9e78d0
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 14:06:49 -0300

    Adicionar pesquisar categoria no serviço de categorias

--- src/main/java/br/com/zup/zupcontatos/services/CategoriaService.java
+++ src/main/java/br/com/zup/zupcontatos/services/CategoriaService.java
@@ -4,6 +4,8 @@ import br.com.zup.zupcontatos.models.CategoriaModel;
 import br.com.zup.zupcontatos.repositories.CategoriaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CategoriaService {
     private CategoriaRepository categoriaRepository;
@@ -18,5 +20,9 @@ public class CategoriaService {
 
     public Iterable <CategoriaModel> listarTodasAsCategorias() {
         return categoriaRepository.findAll();
+    }
+
+    public Optional <CategoriaModel> pesquisarCategoriaPeloId(int id) {
+        return categoriaRepository.findById(id);
     }
 }
