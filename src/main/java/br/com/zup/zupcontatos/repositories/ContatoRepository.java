1aa4137f455b05750aa28fecfb9fdb514d5e8308
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 15:43:46 -0300

    Adicionar os filtros no repositório do contato

--- src/main/java/br/com/zup/zupcontatos/repositories/ContatoRepository.java
+++ src/main/java/br/com/zup/zupcontatos/repositories/ContatoRepository.java
@@ -4,4 +4,6 @@ import br.com.zup.zupcontatos.models.ContatoModel;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ContatoRepository extends CrudRepository <ContatoModel, Integer> {
+    Iterable <ContatoModel> findAllByListaDeProdutos_id(int id);
+    Iterable <ContatoModel> findAllByListaDeProdutos_listaDeCategorias_id(int id);
 }
