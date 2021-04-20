c50acdce2b79ddfdde02d8215fb9cc6786381148
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 16:01:55 -0300

    Adicionar classe para representar cada erro individual do sistema

---
+++ src/main/java/br/com/zup/zupcontatos/exceptions/Erro.java
@@ -0,0 +1,30 @@
+package br.com.zup.zupcontatos.exceptions;
+
+public class Erro {
+    private String campo;
+    private String mensagem;
+
+    public Erro() {
+    }
+    
+    public Erro(String campo, String mensagem) {
+        this.campo = campo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
