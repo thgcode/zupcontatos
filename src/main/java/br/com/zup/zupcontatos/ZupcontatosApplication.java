3db269ce44596c73c4fe066edb0cdf57c901745e
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-20 13:28:16 -0300

    Primeiro commit com as configurações do projeto do spring initializer

---
+++ src/main/java/br/com/zup/zupcontatos/ZupcontatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupcontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupcontatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupcontatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/zupcontatos/ZupcontatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupcontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupcontatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
