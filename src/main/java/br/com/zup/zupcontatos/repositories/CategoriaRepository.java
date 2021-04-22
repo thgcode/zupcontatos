c5fe810500e48e8565f23f61b189812ee1f529c2
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-22 14:54:05 -0300

    Adicionar método para pesquisar categoria pelo nome no repositório

--- src/main/java/br/com/zup/zupcontatos/repositories/CategoriaRepository.java
+++ src/main/java/br/com/zup/zupcontatos/repositories/CategoriaRepository.java
@@ -3,5 +3,8 @@ package br.com.zup.zupcontatos.repositories;
 import br.com.zup.zupcontatos.models.CategoriaModel;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoriaRepository extends CrudRepository <CategoriaModel, Integer> {
+    Optional<CategoriaModel> findByNome(String nome);
 }
