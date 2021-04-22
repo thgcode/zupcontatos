5f1609ef13e90efab637c5f5f4b6e88087159ebe
Thiago Seus <thiago.seus@yahoo.com.br>
2021-04-22 16:06:46 -0300

    Adicionar a documentação do serviço do produto

--- src/main/java/br/com/zup/zupcontatos/services/ProdutoService.java
+++ src/main/java/br/com/zup/zupcontatos/services/ProdutoService.java
@@ -18,6 +18,13 @@ public class ProdutoService {
         this.categoriaService = categoriaService;
     }
 
+    /**
+     * Cadastra um produto no sistema
+     *
+     * @param produto o produto a ser cadastrado
+     *
+     * @return o produto devidamente salvo no banco de dados
+     */
     public ProdutoModel cadastrarProduto(ProdutoModel produto) {
         Optional <ProdutoModel> optionalProduto = produtoRepository.findByNome(produto.getNome());
 
@@ -32,6 +39,14 @@ public class ProdutoService {
         return produtoRepository.save(produto);
     }
 
+    /**
+     * Verifica se o produto já está inserido numa determinada categoria
+     *
+     * @param produto o produto (do banco de dados) a verificar
+     * @param categoria o id da categoria a verificar
+     *
+     * @return true se o produto está nessa categoria, false caso contrário
+     */
     private boolean produtoJaTemACategoria(ProdutoModel produto, CategoriaModel categoria) {
         for (CategoriaModel categoriaPesquisar: produto.getListaDeCategorias()) {
             if (categoriaPesquisar.getId() == categoria.getId()) {
@@ -42,6 +57,14 @@ public class ProdutoService {
         return false;
     }
 
+    /**
+     * Atualiza o produto com uma categoria nova
+     *
+     * @param existente o produto a ser atualizado
+     * @param novaCategoria a categoria que vai ser inserida no produto
+     *
+     * @return o produto devidamente atualizado
+     */
     private ProdutoModel atualizarProdutoComNovaCategoria(ProdutoModel existente, ProdutoModel novaCategoria) {
         CategoriaModel categoria = categoriaService.pesquisarCategoriaPeloId(novaCategoria.getListaDeCategorias().get(0).getId());
 
@@ -54,10 +77,22 @@ public class ProdutoService {
         return existente;
     }
 
+    /**
+     * Lista os produtos cadastrados no sistema
+     *
+     * @return um interável contendo os produtos cadastrados
+     */
     public Iterable <ProdutoModel> listarProdutos() {
         return produtoRepository.findAll();
     }
 
+    /**
+     * Pesquisa um produto através do id, ou joga uma exceção caso não encontre
+     *
+     * @param id o id do produto a ser pesquisado
+     *
+     * @return objeto produto
+     */
     public ProdutoModel pesquisarProdutoPeloId(int id) {
         Optional <ProdutoModel> optionalProduto = produtoRepository.findById(id);
 
